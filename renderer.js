76bcde922f5d53184cb15b5c8e8aa78ab2f53e83
magiobus <aalbertobus@gmail.com>
2017-03-26 15:51:22 -0600

    More products in generator

--- renderer.js
+++ renderer.js
@@ -12,14 +12,41 @@ const productList = [
   { name: 'Pizza', price: 47.17 },
   { name: 'Torta', price: 4.56 },
   { name: 'Paracetamol', price: 5.76 },
-  { name: 'Coca Cola', price: 7.35 }
+  { name: 'Coca Cola', price: 7.35 },
+  { name: 'Pepsi', price: 6.32 },
+  { name: 'Tortillas', price: 16.05 },
+  { name: 'Queso', price: 56.87 },
+  { name: 'Mango', price: 8.56 },
+  { name: 'Naranja', price: 6.45 },
+  { name: 'Aguacate', price: 16.78 },
+  { name: 'Chocolate', price: 10.25 },
+  { name: 'Pan', price: 3.57 },
+  { name: 'Jabon', price: 5.78 },
+  { name: 'Shampoo', price: 38.08 },
+  { name: 'Acondicionador', price: 40.08 },
+  { name: 'Elote', price: 8.07 },
+  { name: 'Trapeador', price: 48.90 },
+  { name: 'Foco', price: 18.08 },
+  { name: 'Donitas', price: 12.29 },
+  { name: 'Tornillos', price: 8.90 },
+  { name: 'Sandia', price: 20.08 },
+  { name: 'Papaya', price: 10.24 },
+  { name: 'Plumones', price: 24.78 },
+  { name: 'Vino', price: 160.08 },
+  { name: 'Cerveza', price: 24.23 },
+  { name: 'Jamon', price: 48.23 },
+  { name: 'Cerveza', price: 24.23 },
+  { name: 'Yogurth', price: 23.23 },
+  { name: 'Dulces', price: 35.23 },
+  { name: 'Paleta', price: 15.34 },
+  { name: 'Helado', price: 14.34 }
 ]
 
 const getRamdomItem = () => productList[_.random(0, productList.length - 1)]
 
 const getRamdomList = () => {
   let tmp = []
-  let times = _.random(3, 8)
+  let times = _.random(8, 34)
 
   for (var i = 0; i < times; i++) {
     tmp.push(getRamdomItem())
